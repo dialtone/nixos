ed7217d4ebcded648c60c39013df3703ecf66f31
Valentino Volonghi <dialtone@gmail.com>
2023-11-25 16:23:58 -0800

    try to add parameters at random

--- hosts/dabass/default.nix
+++ hosts/dabass/default.nix
@@ -8,16 +8,44 @@
       luks.enable = false;
     };
   };
+
+  hardware.opengl.enable = true;                                                                                                                                                                              hardware.opengl.driSupport = true;
+
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
-  boot.kernelParams = [ ];
+  boot.kernelParams = [ "pcie_aspm=force" "consoleblank=60" ];
   networking.hostId = "7af06fcb";
   networking.hostName = "dabass";
   time.timeZone = "America/Los_Angeles";
+
+  powerManagement.powertop.enable = true;
+
 
   # import preconfigured profiles
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     # (modulesPath + "/profiles/hardened.nix")
     # (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+
+  environment.systemPackages = with pkgs; [
+    pciutils
+    hdparm
+    hd-idle
+    hddtemp
+    smartmontools
+    go
+    gotools
+    gopls
+    go-outline
+    gocode
+    gopkgs
+    gocode-gomod
+    godef
+    golint
+    powertop
+    cpufrequtils
+    gnumake
+    gcc
+    intel-gpu-tools
   ];
 }
